3f7dc654b72b78e3e6826adafe734579f3f19f09
BurningFlare <floopypiper@gmail.com>
2025-03-14 22:47:05 -0700

    player movement

---
+++ Assets/Scripts/PlayerInput/PlayerControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    private Vector2 _moveInput;
+    private Rigidbody2D _rb;
+    private PlayerInput _playerInput;
+    
+   
+
+    public ActionInputPlayerThing PlayerInputActions {get; private set;}
+    
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
+        PlayerInputActions = new ActionInputPlayerThing();
+        BindControls();
+        PlayerInputActions.Player.Enable();
+    }
+
+
+    private void BindControls()
+    {
+        Debug.Log("bindcontrols");
+        PlayerInputActions.Player.W.performed += context => moveSomewhere(context, new Vector2(0,16), true);
+        PlayerInputActions.Player.W.canceled += context => moveSomewhere(context, new Vector2(0, 16), false);
+        PlayerInputActions.Player.S.performed += context => moveSomewhere(context, new Vector2(0, -16), true);
+        PlayerInputActions.Player.S.canceled += context => moveSomewhere(context, new Vector2(0, -16), false);
+        PlayerInputActions.Player.A.performed += context => moveSomewhere(context, new Vector2(-16, 0), true);
+        PlayerInputActions.Player.A.canceled += context => moveSomewhere(context, new Vector2(-16, 0), false);
+        PlayerInputActions.Player.D.performed += context => moveSomewhere(context, new Vector2(16, 0), true);
+        PlayerInputActions.Player.D.canceled += context => moveSomewhere(context, new Vector2(16, 0), false);
+    }
+
+    private void moveSomewhere(InputAction.CallbackContext context, Vector2 direction,bool isPressed)
+    {
+        if (isPressed)
+        {
+            Debug.Log("PRESSED KEY");
+            _rb.MovePosition(_rb.position + direction * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("LET GO OF KEY");
+            _rb.MovePosition(_rb.position + direction * Time.deltaTime);
+        }
+    }
+}
