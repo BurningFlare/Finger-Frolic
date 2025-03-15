1fb0574c043100cc64913ae3e892cc5c3919eb87
The64thRealm <155809277+WaltenChan64@users.noreply.github.com>
2025-03-15 01:19:35 -0700

    player prefab updated

--- Assets/Scripts/PlayerInput/WalkableTiles.cs
+++ Assets/Scripts/PlayerInput/WalkableTiles.cs
@@ -4,7 +4,6 @@ public class WalkableTiles : MonoBehaviour
 {
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("hi2");
         if (collision.gameObject.CompareTag("WalkableTilemap"))
         {
             Debug.Log("OFF PATH");
