6dd8b241a4bd34fac8b668c03df3ce50819b389a
BurningFlare <floopypiper@gmail.com>
2025-03-14 23:44:34 -0700

    player movement

--- Assets/Scripts/PlayerInput/PlayerControls.cs
+++ Assets/Scripts/PlayerInput/PlayerControls.cs
@@ -3,15 +3,32 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewMonoBehaviour : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private PlayerInput _playerInput;
     
-  
+    public static PlayerControls Instance { get; private set; }
     public ActionInputPlayerThing PlayerInputActions {get; private set;}
-    
 
+    ControlToggler WPress;
+    ControlToggler SPress;
+    ControlToggler APress;
+    ControlToggler DPress;
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        WPress = new ControlToggler(new Vector2(0, 1));
+        SPress = new ControlToggler(new Vector2(0, -1));
+        APress = new ControlToggler(new Vector2(-1, 0));
+        DPress = new ControlToggler(new Vector2(1, 0));
+    }
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -21,18 +38,37 @@ public class NewMonoBehaviour : MonoBehaviour
         PlayerInputActions.Player.Enable();
     }
 
+    public class ControlToggler
+    {
+        System.Action<InputAction.CallbackContext,bool> callback;
+        bool keyPressed = false;
+
+        public ControlToggler(Vector2 direction)
+        {
+            callback = (context,isPressed) => PlayerControls.Instance.moveSomewhere(context, direction, isPressed);
+        }
+
+        public void checkOnOrOff(bool p, InputAction.CallbackContext contextOnOrOff)
+        {
+            if(keyPressed!=p)
+            {
+                keyPressed = p;
+                callback(contextOnOrOff,keyPressed);
+            } 
+        }
+
+    }
 
     private void BindControls()
     {
-        Debug.Log("bindcontrols");
-        PlayerInputActions.Player.W.performed += context => moveSomewhere(context, new Vector2(0,1), true);
-        PlayerInputActions.Player.W.canceled += context => moveSomewhere(context, new Vector2(0, 1), false);
-        PlayerInputActions.Player.S.performed += context => moveSomewhere(context, new Vector2(0, -1), true);
-        PlayerInputActions.Player.S.canceled += context => moveSomewhere(context, new Vector2(0, -1), false);
-        PlayerInputActions.Player.A.performed += context => moveSomewhere(context, new Vector2(-1, 0), true);
-        PlayerInputActions.Player.A.canceled += context => moveSomewhere(context, new Vector2(-1, 0), false);
-        PlayerInputActions.Player.D.performed += context => moveSomewhere(context, new Vector2(1, 0), true);
-        PlayerInputActions.Player.D.canceled += context => moveSomewhere(context, new Vector2(1, 0), false);
+        PlayerInputActions.Player.W.performed += context => WPress.checkOnOrOff(true,context);
+        PlayerInputActions.Player.W.canceled += context => WPress.checkOnOrOff(false, context);
+        PlayerInputActions.Player.S.performed += context => SPress.checkOnOrOff(true, context);
+        PlayerInputActions.Player.S.canceled += context => SPress.checkOnOrOff(false, context);
+        PlayerInputActions.Player.A.performed += context => APress.checkOnOrOff(true, context);
+        PlayerInputActions.Player.A.canceled += context => APress.checkOnOrOff(false, context);
+        PlayerInputActions.Player.D.performed += context => DPress.checkOnOrOff(true, context);
+        PlayerInputActions.Player.D.canceled += context => DPress.checkOnOrOff(false, context);
     }
 
     private void moveSomewhere(InputAction.CallbackContext context, Vector2 direction,bool isPressed)
@@ -44,7 +80,7 @@ public class NewMonoBehaviour : MonoBehaviour
         }
         else
         {
-            Debug.Log("LET GO OF KEY");
+            Debug.Log("RELEASED KEY");
             _rb.MovePosition(_rb.position + direction);
         }
     }
